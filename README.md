698204bc585ecf6185e5807ee0da40b65fc476b1
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-23 21:26:29 -0800

    add java options and branching

---
+++ README.md
@@ -0,0 +1,15 @@
+# Survey
+
+#### It determines which language is the best for you
+
+#### By Ivan Ramos
+
+## if you choose for passion and exprience as yes it will lead you to C#, or if you choose those as 'No' it willlead you to Python, anything else like "NA"/No prefrence will lead you to Java
+
+## Any more bugs please let me know
+
+
+### Ivan Ramos Survey 
+
+
+Copyright (c) 2017 **_{Ivan Ramos}_**
---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    $('#csharp').hide();
+    $('#Java').hide();
+    $('#Python').hide();
+
+
+    var exprience = $("select#exprience").val();
+    var passion = $("select#passion").val();
+    var years = $("select#years").val();
+    var prefrence = $("select#prefrence").val();
+
+    if (exprience === 'yes' && passion === 'yes') {
+      $('#c#').show();
+      } else if (exprience === 'no' && passion === 'no') {
+        $('#Python').show();
+      } else {
+        $('#Java').show();
+
+    }
+  });
+});
