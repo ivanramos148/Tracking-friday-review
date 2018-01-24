4dfd058682b3001e9632dedbe74c5564d60f0510
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-23 22:09:04 -0800

    add another select option

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
     $('#Java').hide();
     $('#Python').hide();
 
-
     var exprience = $("select#exprience").val();
     var passion = $("select#passion").val();
     var years = $("select#years").val();
     var prefrence = $("select#prefrence").val();
+    var career = $("select#career").val();
+
 
     if (exprience === 'yes' && passion === 'yes') {
-      $('#c#').show();
+      $('#csharp').show();
       } else if (exprience === 'no' && passion === 'no') {
         $('#Python').show();
       } else {
